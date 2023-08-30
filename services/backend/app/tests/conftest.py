a69a2acd60e23c21f5e913364b9d096a37e26508
pavdwest <pavdwesthuizen@gmail.com>
2023-08-30 20:58:55 +0200

    Stash

--- services/backend/app/tests/conftest.py
+++ services/backend/app/tests/conftest.py
@@ -1,8 +1,15 @@
+import os
+
 import pytest
 from asgi_lifespan import LifespanManager
 from httpx import AsyncClient
 
 from src.main import app
+
+
+# Config env vars
+DATABASE_NAME: str  = os.environ.get('DATABASE_NAME')
+os.environ['DATABASE_NAME'] = f"{DATABASE_NAME}_test"
 
 
 @pytest.fixture(scope='session')
