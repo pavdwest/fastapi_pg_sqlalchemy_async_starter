9fe220557f45d7c5d8c66c40e284f40e76eb7d27
pavdwest <pavdwesthuizen@gmail.com>
2023-12-20 17:53:23 +0100

    Minor refactor.

--- services/backend/app/src/modules/home/routes.py
+++ services/backend/app/src/modules/home/routes.py
@@ -3,7 +3,7 @@ from typing import Dict
 from fastapi import APIRouter, status, Response
 
 from src.versions import ApiVersion
-from src.queue import queue_init
+from src.modules.arqueue.bus import Bus
 
 
 router = APIRouter(
@@ -30,10 +30,9 @@ async def home(response: Response) -> Dict:
             'message': 'Hello boils and ghouls'
         }
     """
-    queue = await queue_init()
 
     for url in ('https://facebook.com', 'https://microsoft.com', 'https://github.com'):
-        await queue.enqueue_job('download_content', url)
+        await Bus.queue.enqueue_job('download_content', url)
 
     return {
         'message': 'Hello boils and ghouls'
